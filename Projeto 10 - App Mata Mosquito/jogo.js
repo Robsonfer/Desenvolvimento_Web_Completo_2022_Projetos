f2b2d15b16814c5f735a8363920ab027e238b0a5
Robson <76750320+Robsonfer@users.noreply.github.com>
2023-10-02 21:18:10 -0300

    tela game over mata mosquito

--- Projeto 10 - App Mata Mosquito/jogo.js
+++ Projeto 10 - App Mata Mosquito/jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -12,6 +13,20 @@ function ajustaTamanhoPalcoJogo() {
 ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
+
+
+	// Remover o mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+
+		if(vidas > 3){
+			window.location.href = 'fim_de_jogo.html'
+		} else {
+			document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+			vidas ++
+		}
+	}
+
 	// Criando posições aleatórias
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -21,14 +36,49 @@ function posicaoRandomica() {
 
 	console.log(posicaoX, posicaoY)
 
-	//Criando o elemento html
+	// Criando o elemento html
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosquito.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+
+	// Criando a parte CSS do elemento
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 
+	// Criando um id para retirar o mosquito da tela antes da criação automática de um novo
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
+
 	document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+	}
+}
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+
+		case 1:
+			return 'ladoB'
+	}
 }
 
